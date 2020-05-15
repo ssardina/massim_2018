e771ea0ca55c4d14ba22f64d70f48787db73cf41
Sebastian <ssardina@gmail.com>
2020-05-15 16:09:23 +1000

    Allow Logger to be disabled/enabled

--- eismassim/src/main/java/massim/eismassim/Log.java
+++ eismassim/src/main/java/massim/eismassim/Log.java
@@ -4,8 +4,17 @@ package massim.eismassim;
  * Very simple Logger.
  */
 public abstract class Log {
+    private static boolean logOn = true;
 
     public static void log(String message){
-        System.out.println(message);
+        if (logOn) System.out.println(message);
+    }
+
+    public boolean isLogOn() {
+        return logOn;
+    }
+
+    public void setLogOn(boolean logOn) {
+        this.logOn = logOn;
     }
 }
